1c5cf9b99d24f8df3248861c843c8cca79e078fa
Arin Sinabian <arin.sinabian@charge-amps.com>
2022-10-29 21:10:07 +0200

    refactor

--- pages/components/AboutMe.tsx
+++ pages/components/AboutMe.tsx
@@ -3,32 +3,31 @@
 
 import React from "react";
 import { PageContext } from "..";
-import { Cv, AboutMe as a,  } from "../api/cv";
+import { Cv, AboutMe as a } from "./../cv-data/cv";
 
-export default function AboutMe({cv, lang}: {cv:Cv, lang?:string})
-{
-  return (<PageContext.Consumer> 
-  { 
+export default function AboutMe({ cv, lang }: { cv: Cv, lang?: string }) {
+  return (<PageContext.Consumer>
+    {
       (settings) => {
-          let choosenLang = settings.settings.lang;
-            if(lang){
-                choosenLang = lang;
-            }
-          const languageVersion:a = cv && cv[choosenLang];
-          return (
-              
-              <div className="" data-aos="fade-left">
-                <h2 className="text-2xl font-bold pb-4">
-                  {choosenLang === "sv" ? "Profil" : "Profile"}
-                </h2>
-                  
-                <p>
-                  {languageVersion?.aboutMe}
-                </p>
-              </div>
-        
-          );
+        let choosenLang = settings.settings.lang;
+        if (lang) {
+          choosenLang = lang;
+        }
+        const languageVersion: a = cv && cv[choosenLang];
+        return (
+
+          <div className="" data-aos="fade-left">
+            <h2 className="text-2xl font-bold pb-4">
+              {choosenLang === "sv" ? "Profil" : "Profile"}
+            </h2>
+
+            <p>
+              {languageVersion?.aboutMe}
+            </p>
+          </div>
+
+        );
       }
-      }
-      </PageContext.Consumer>);
+    }
+  </PageContext.Consumer>);
 }
